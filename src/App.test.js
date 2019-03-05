5c0f1e0329cbd51f85d13f13cef74ea7b6246212
Josh Van Wyk <joshvanwyk@ymail.com>
2019-03-05 01:51:08 -0500

    Update App.test w/ CircleCi

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>,
+    div
+  );
+  serviceWorker.unregister();
 });
